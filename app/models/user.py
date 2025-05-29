b88a39d767c1c8e68b1249af66e779fddeaf6330
YingXuye <23302010042@m.fudan.edu.cn>
2025-05-29 21:39:18 +0800

    fix: Clean up formatting and improve readability in user model classes

--- app/models/user.py
+++ app/models/user.py
@@ -20,7 +20,7 @@ class User:
     def __repr__(self) -> str:
         """Representation of the User."""
         return f"<User {self.username}>"
-    
+
     def asdict(self):
         return asdict(self)
 
@@ -37,10 +37,11 @@ class Admin(User):
     def __repr__(self) -> str:
         """Representation of the Admin."""
         return f"<Admin {self.username}>"
-    
+
     def asdict(self, only_parent=False):
         if only_parent:
-            parent_attrs = {attr for attr in vars(User(self.user_id, self.name, self.username, self.password, self.phone, self.email, self.address, self.discriminator))}
+            parent_attrs = {attr for attr in vars(User(
+                self.user_id, self.name, self.username, self.password, self.phone, self.email, self.address, self.discriminator))}
             return {k: v for k, v in asdict(self).items() if k in parent_attrs}
         return asdict(self)
 
@@ -53,9 +54,11 @@ class Staff(User):
     hourly_rate: int = 0
 
     def __init__(self, **kwargs):
-        kwargs["user_id"] = kwargs["staff_id"]
+        kwargs["user_id"] = kwargs["staff_id"] if "staff_id" in kwargs else kwargs.get(
+            "user_id", None)
         # print(kwargs)
-        cleared_kwargs = {key:kwargs[key] for key in kwargs.keys() if not (key in ["staff_id", "jobtype", "hourly_rate"])}
+        cleared_kwargs = {key: kwargs[key] for key in kwargs.keys() if not (
+            key in ["staff_id", "jobtype", "hourly_rate"])}
         print(cleared_kwargs)
         super().__init__(**cleared_kwargs)
         self.staff_id = kwargs.get("staff_id", self.user_id)
@@ -73,10 +76,11 @@ class Staff(User):
         if self.repair_assignments:
             return sum(assignment.time_worked for assignment in self.repair_assignments if assignment.time_worked is not None)
         return 0.0
-    
+
     def asdict(self, only_parent=False):
         if only_parent:
-            parent_attrs = {attr for attr in vars(User(self.user_id, self.name, self.username, self.password, self.phone, self.email, self.address, self.discriminator))}
+            parent_attrs = {attr for attr in vars(User(
+                self.user_id, self.name, self.username, self.password, self.phone, self.email, self.address, self.discriminator))}
             return {k: v for k, v in asdict(self).items() if k in parent_attrs}
         return asdict(self)
 
@@ -94,9 +98,10 @@ class Customer(User):
     def __repr__(self) -> str:
         """Representation of the Customer."""
         return f"<Customer {self.username}>"
-    
+
     def asdict(self, only_parent=False):
         if only_parent:
-            parent_attrs = {attr for attr in vars(User(self.user_id, self.name, self.username, self.password, self.phone, self.email, self.address, self.discriminator))}
+            parent_attrs = {attr for attr in vars(User(
+                self.user_id, self.name, self.username, self.password, self.phone, self.email, self.address, self.discriminator))}
             return {k: v for k, v in asdict(self).items() if k in parent_attrs}
         return asdict(self)
